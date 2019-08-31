2cdf941b619f0511dd5ef2c576b66f09fe2b7585
Nikita Didenko <5773480@gmail.com>
2019-08-31 18:59:23 +0300

    added age search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 
 class HomeController < ApplicationController
   before_action :set_user, except: :front
+
   respond_to :html, :js
 
   def index
@@ -14,7 +15,15 @@ class HomeController < ApplicationController
   end
 
   def find_friends
-    @users = User.where("name ~* ?", params[:search_name]) if params[:search_name].present?
+    age_range = validate_age(params)
+    min_dob = Time.now - age_range[1].years
+    max_dob = Time.now - age_range[0].years
+    if params[:search_name].present?
+      @users = User.where("name ~* ?", params[:search_name])
+                   .where(dob: min_dob..max_dob)
+    else
+      @users = User.where(dob: min_dob..max_dob)
+    end
     @users = (User.where.not(id: current_user.id) - current_user.blocks) if @users.nil?
     @users = @users.paginate(page: params[:page], per_page: 10)
     # @users = User.where("name ~* ?", params[:search]) if params[:search].present?
@@ -33,5 +42,17 @@ class HomeController < ApplicationController
   private
   def set_user
     @user = current_user
+  end
+
+  def validate_age(params)
+    age_min = params[:search_age_min].to_i
+    age_max = params[:search_age_max].to_i
+    default_range = [0, 120]
+    age_max = 120 if age_max == 0
+    return default_range if age_min < 0 || age_max < 0
+    return default_range if age_max < age_min
+    default_range[0] = age_min
+    default_range[1] = age_max
+    default_range
   end
 end
